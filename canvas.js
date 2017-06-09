f32c67613355b9042298763df4a22546fdff613b
Kirk <kirkdmcm@gmail.com>
2017-06-09 16:33:01 -0600

    prodev WIP

--- canvas.js
+++ canvas.js
@@ -1,10 +1,15 @@
+const jq = require('jquery');
 const pdfjs = require('pdfjs-dist');
 const fs = require('fs');
+var $ = jq;
 
 document.addEventListener('DOMContentLoaded', function () {
     console.log('document is ready. I can sleep now');
 
     var testFile = new Uint8Array(fs.readFileSync('pdf-sample.pdf'));
+    var box = $('#box');
+
+    box.append('test 123');
 
     pdfjs.getDocument(testFile);
     // .then(function (pdfDocument) {
